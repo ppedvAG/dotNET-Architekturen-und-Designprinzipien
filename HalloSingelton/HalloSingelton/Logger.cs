ca1bc530fbc7a26c3d160652b09468a51c6b5d7a
André <ruland@outlook.de>
2020-04-20 09:54:21 +0200

    Singelton ohne Threadsafe

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HalloSingelton
+{
+    public class Logger
+    {
+
+        private static Logger instance = null;
+        public static Logger Instance
+        {
+            get {
+                if (instance == null)
+                    instance = new Logger();
+
+                return instance;
+            }
+        }
+
+
+        private Logger()
+        {
+            Log("Neuer Logger gestartet");
+        }
+
+        public void Log(string txt, LogLevel level = LogLevel.Info)
+        {
+            var oldCol = Console.ForegroundColor;
+
+            Console.ForegroundColor = level switch
+            {
+                LogLevel.Error => ConsoleColor.Red,
+                LogLevel.Warn => ConsoleColor.DarkYellow,
+                _ => ConsoleColor.Green,
+            };
+
+            Console.WriteLine($"[{DateTime.Now}] {txt}");
+            Console.ForegroundColor = oldCol;
+        }
+    }
+
+    public enum LogLevel
+    {
+        Info,
+        Warn,
+        Error
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Singelton!");
+
+            //var logger = new Logger();
+            
+
+            Logger.Instance.Log("Logger Info TEST");
+            Logger.Instance.Log("Logger Warnung TEST", LogLevel.Warn);
+            Logger.Instance.Log("Logger Fehler TEST", LogLevel.Error);
+
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
